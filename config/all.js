73ed2ebb5cddfbdc4f75b5dcbbf83936b704badc
Chris Spiegl <chris@chrisspiegl.com>
2020-05-18 19:27:01 +0200

    Added YouTube API Scopes.

--- config/all.js
+++ config/all.js
@@ -89,6 +89,9 @@ module.exports = {
       scope: [
         'https://www.googleapis.com/auth/userinfo.profile',
         'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/youtube.readonly',
+        'https://www.googleapis.com/auth/youtube',
+        'https://www.googleapis.com/auth/youtube.force-ssl',
       ],
     },
     telegram: {
