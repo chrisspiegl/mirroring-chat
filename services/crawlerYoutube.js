3543fabbdb80789a0b76ae030e8dddbea826aaaf
Chris Spiegl <chris@chrisspiegl.com>
2020-05-24 12:16:48 +0200

    Implementing the redisKeyGenerator and the new database driven message in the YouTube Crawler. Also added cron based crawling for broadcasts (upcoming and active) as well as chat messages. Chat messages are crawled once per minute, upcoming broadcasts are crawled once every hour, and active streams are looked for every 5 minutes.

--- services/crawlerYoutube.js
+++ services/crawlerYoutube.js
@@ -7,38 +7,119 @@ log.log = console.log.bind(console)
 // eslint-disable-next-line no-unused-vars
 const error = debug(`${config.slug}:botYoutube:error`)
 
+const pnotice = require('pushnotice')(`${config.slug}:botYoutube`, {
+  env: config.env,
+  chat: config.pushnotice.chat,
+  debug: true,
+  disabled: config.pushnotice.disabled,
+})
+
 const moment = require('moment-timezone')
+const { CronJob } = require('cron')
 
 const messagesStore = require('server/messagesStore')
 const models = require('database/models')
-const youtubeService = require('./youtubeService')
+const YoutubeService = require('./youtubeService')
 
 // Lot's of notes taken from: https://medium.com/@jonnykalambay/doing-it-live-learn-youtubes-api-by-making-a-chatbot-d55e2156715f
 // TODO: improve polling to include error handling, get inspiration from: https://github.com/JamesFrost/youtube-stream/tree/20291650f92bb7ed2acd52d9c4773843bb7bb155
 
-const init = async () => {
-  await models.init()
+const crawlUserBroadcasts = async (userProvider, broadcastStatus) => {
+  log(`${userProvider.displayName}: crawlUserBroadcasts for ${broadcastStatus} broadcasts`)
+  const youtubeService = new YoutubeService(userProvider)
+  await youtubeService.authorize()
+  const broadcasts = await youtubeService.findBroadcasts(broadcastStatus)
+  log(`${userProvider.displayName}: found ${broadcasts.length} ${broadcastStatus} broadcasts`)
+  broadcasts.forEach(async (broadcast) => {
+    const broadcastOptions = {
+      idChatProvider: broadcast.id,
+      idUser: userProvider.idUser,
+      provider: 'youtube',
+      title: broadcast.snippet.title,
+      providerObject: broadcast,
+      status: (broadcast.snippet.isDefaultBroadcast) ? 'permanent' : broadcastStatus,
+      isPermanent: broadcast.snippet.isDefaultBroadcast,
+      isTracked: true,
+    }
+    let broadcastInDatabase = await models.Chat.findOne({
+      where: {
+        idChatProvider: broadcastOptions.idChatProvider,
+      },
+    })
 
-  const userProvider = await models.UserGoogle.findByPk('104111025133854439780')
+    if (broadcastInDatabase) {
+      broadcastInDatabase = await broadcastInDatabase.update(broadcastOptions)
+      log(`${userProvider.displayName}: updating broadcast ${broadcastOptions.title}`)
+    } else {
+      broadcastInDatabase = await models.Chat.create(broadcastOptions)
+      log(`${userProvider.displayName}: creating broadcast ${broadcastOptions.title}`)
+    }
+  })
+}
 
-  await youtubeService.authorize(userProvider)
-  // TODO: Scan for new active live streams with chats in a certain interval instead of just once.
-  const liveBroadcasts = await youtubeService.findActiveChat()
-  liveBroadcasts.forEach((broadcast) => {
-    youtubeService.startTrackingChat(broadcast, true, (messages) => {
-      messages.forEach((message) => {
-        messagesStore.add(userProvider.idUser, {
-          id: message.etag,
-          provider: 'youtube',
-          timestamp: moment(message.snippet.publishedAt).unix(),
-          username: message.authorDetails.displayName,
-          displayName: message.authorDetails.displayName,
-          message: message.snippet.textMessageDetails.messageText,
-          providerObject: message,
-        })
+const runCrawlUserBroadcasts = (broadcastStatus) => {
+  log(`init crawler for ${broadcastStatus} user broadcasts`)
+  return async () => {
+    log(`run crawler for ${broadcastStatus} user broadcasts`)
+    const users = await models.UserGoogle.findAll()
+    await users.forEach(async (userProvider) => {
+      await crawlUserBroadcasts(userProvider, broadcastStatus)
+    })
+    log(`finished crawler for ${broadcastStatus} user broadcasts`)
+  }
+}
+
+const runCrawlChats = async () => {
+  const chats = await models.Chat.findAll({
+    where: {
+      provider: 'youtube',
+      isTracked: true,
+    },
+    include: [
+      {
+        model: models.User,
+        include: [models.UserGoogle],
+      },
+    ],
+  })
+  log(`run chat crawler for ${chats.length} broadcasts`)
+  await chats.forEach(async (chat) => {
+    const userProvider = chat.User.UserGoogle
+    log(`${userProvider.displayName}: crawl chat ${chat.title} for new messages`)
+    const youtubeService = new YoutubeService(userProvider)
+    await youtubeService.authorize()
+
+    const newMessages = await youtubeService.getChatMessages(chat.providerObject)
+    log(`${userProvider.displayName}: found ${newMessages.length} new messages for chat ${chat.title}`)
+    newMessages.forEach((message) => {
+      console.log('runCrawlChats -> message', message)
+      messagesStore.addMessage({
+        idChatMessageProvider: message.id,
+        idChat: chat.idChat,
+        idUser: userProvider.idUser,
+        displayName: message.authorDetails.displayName,
+        message: message.snippet.displayMessage,
+        provider: 'youtube',
+        providerObject: message,
+        sentAt: moment(message.snippet.publishedAt),
       })
     })
   })
-  // await youtubeService.stopTrackingChat()
 }
-init()
+
+const init = async () => {
+  await models.init()
+  const jobUserBroadcastsActive = new CronJob('0 */5 * * * *', runCrawlUserBroadcasts('active'), () => log('Cron jobUserBroadcastsActive Finished'), true, 'UTC', this, true)
+  const jobUserBroadcastsUpcoming = new CronJob('0 0 * * * *', runCrawlUserBroadcasts('upcoming'), () => log('Cron jobUserBroadcastsUpcoming Finished'), true, 'UTC', this, true)
+  const jobChat = new CronJob('0 * * * * *', runCrawlChats, () => log('Cron jobChat Finished'), true, 'UTC', this, true)
+}
+
+// Run the init method if the file was called directly and not through 'require'
+if (require.main === module) {
+  init()
+}
+
+process.on('unhandledRejection', async (reason, promise) => {
+  error('unhandledRejection', reason.stack || reason, promise)
+  pnotice(`unhandledRejection:\n${JSON.stringify(reason)}`, 'ERROR')
+})
--- services/youtubeService.js
+++ services/youtubeService.js
@@ -11,61 +11,57 @@ const { google } = require('googleapis')
 
 const { redisClient } = require('server/redis')
 const { refreshTokenAccess } = require('server/passportRefresh')
+const redisKeyGenerator = require('server/redisKeyGenerator')
 
 const { OAuth2 } = google.auth
 const youtube = google.youtube('v3')
 
 const { clientId, clientSecret } = config.passport.google
-const youtubeService = {}
 
-const auth = new OAuth2(clientId, clientSecret)
-
-youtubeService.authorize = async (userProviderParam) => {
-  // Refresh oauth tokens for user
-  const userProvider = await refreshTokenAccess('google', userProviderParam).catch((err) => {
-    error(`Error while refreshing the tokens for youtube ${userProvider.displayName}/${userProvider.idUserProvider}: `, err)
-  })
-  const tokens = {
-    refresh_token: userProvider.tokenRefresh,
-    access_token: userProvider.tokenAccess,
+module.exports = class YouTubeService {
+  constructor(userProvider) {
+    this.userProvider = userProvider
   }
-  auth.setCredentials(tokens)
-  log(`successfully set credentials tokens for ${userProvider.displayName}/${userProvider.idUserProvider}`)
-}
 
-youtubeService.findActiveChat = async () => {
-  // IMPROVE: may be able to improve and only do one request (or make them happen at the same time)
-  const responseUpcoming = await youtube.liveBroadcasts.list({
-    auth,
-    part: 'snippet', // id, snippet, contentDetails, and status.
-    broadcastStatus: 'upcoming',
-    // mine: 'true', == broadcastStatus: 'all'
-  })
-  const responseActive = await youtube.liveBroadcasts.list({
-    auth,
-    part: 'snippet', // id, snippet, contentDetails, and status.
-    broadcastStatus: 'active',
-    // mine: 'true', == broadcastStatus: 'all'
-  })
-  const liveActive = responseActive.data.items
-  const liveUpcoming = responseUpcoming.data.items
-  const liveBroadcasts = liveActive.concat(liveUpcoming)
-  return liveBroadcasts
-}
+  async authorize() {
+    this.auth = new OAuth2(clientId, clientSecret)
+    // Refresh oauth tokens for user
+    this.userProvider = await refreshTokenAccess('google', this.userProvider).catch((err) => {
+      error(`Error while refreshing the tokens for youtube ${this.userProvider.displayName}/${this.userProvider.idUserProvider}: `, err)
+    })
+    const tokens = {
+      refresh_token: this.userProvider.tokenRefresh,
+      access_token: this.userProvider.tokenAccess,
+    }
+    this.auth.setCredentials(tokens)
+    log(`successfully set credentials tokens for ${this.userProvider.displayName}/${this.userProvider.idUserProvider}`)
+  }
 
-youtubeService.trackingChatActive = false
-youtubeService.startTrackingChat = (broadcast, tracking, listener) => {
-  log(`startTrackingChat for ${broadcast.snippet.title}`)
-  youtubeService.trackingChatActive = tracking
+  async findBroadcasts(broadcastStatus = 'active') {
+    // TODO: make it so that eTags are used for caching
+    // Caching can be achieved by storing the youtube.liveBroadcasts.list etag which is in 'response.data.etag'
+    // This is done by: redis key for broadcast list etag, request the list without the "snippet" part, and check if the etag is different, if it is, request with 'snippet' if it is not, then do not request new data.
+    const response = await youtube.liveBroadcasts.list({
+      auth: this.auth,
+      part: 'snippet', // id, snippet, contentDetails, and status.
+      broadcastStatus,
+      maxResults: 50, // TODO: pagination for requesting live streams
+      // mine: true, // == broadcastStatus: 'all'
+    })
+    const broadcasts = response.data.items
+    return broadcasts
+  }
 
-  const getChatMessages = async () => {
+
+  async getChatMessages(broadcast) {
     log(`getChatMessages for ${broadcast.snippet.title}`)
-    const redisKey = `${config.slugShort}:${config.envShort}:youtube:broadcast:nextpage:${broadcast.etag}`
+    const redisKey = redisKeyGenerator.youtube.broadcast.nextPage(broadcast.id)
     const nextPage = await redisClient.getAsync(redisKey) || ''
     const response = await youtube.liveChatMessages.list({
-      auth,
+      auth: this.auth,
       part: 'snippet,authorDetails',
       liveChatId: broadcast.snippet.liveChatId,
+      maxResults: 2000,
       pageToken: nextPage,
     })
     const { data } = response
@@ -74,35 +70,25 @@ youtubeService.startTrackingChat = (broadcast, tracking, listener) => {
       log('redis:set:nextPage', { err, results })
     })
     log(`received new messages and instructed to poll again in ${data.pollingIntervalMillis / 1000} seconds`)
-    // if (youtubeService.trackingChatActive) setTimeout(getChatMessages, data.pollingIntervalMillis)
-    if (youtubeService.trackingChatActive) setTimeout(getChatMessages, 1000 * 60) // polling once per minute
-    if (newMessages.length > 0) listener(newMessages)
+    return newMessages
   }
-  getChatMessages()
-  // setInterval(getChatMessages, 5000) // could use standard interval, currently relying on suggested interval based on youtube reply
-}
 
-youtubeService.stopTrackingChat = () => {
-  youtubeService.trackingChatActive = false
-}
-
-youtubeService.insertMessage = (broadcast, messageText) => {
-  youtube.liveChatMessages.insert({
-    auth,
-    part: 'snippet',
-    resource: {
-      snippet: {
-        type: 'textMessageEvent',
-        liveChatId: broadcast.snippet.liveChatId,
-        textMessageDetails: {
-          messageText,
+  insertMessage(broadcast, messageText) {
+    youtube.liveChatMessages.insert({
+      auth: this.auth,
+      part: 'snippet',
+      resource: {
+        snippet: {
+          type: 'textMessageEvent',
+          liveChatId: broadcast.snippet.liveChatId,
+          textMessageDetails: {
+            messageText,
+          },
         },
       },
     },
-  },
-  () => {
-    log('sent message to youtube')
-  })
+    () => {
+      log('sent message to youtube')
+    })
+  }
 }
-
-module.exports = youtubeService
