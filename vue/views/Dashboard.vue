3ddfaf98c9404e549dc43e5733c34051c6eea736
Chris Spiegl <chris@chrisspiegl.com>
2020-05-25 16:17:08 +0200

    Add dashboard text with instructions.

--- vue/views/Dashboard.vue
+++ vue/views/Dashboard.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   v-container.dashboard
     h1 Dashboard
+    p Check linked accounts on the #[a(href="/account") accounts] page and then head over to the #[a(href="/chat") chat] page to see the incoming messages on your live streaming profiles.
+    p With future develpoment, on this page, there will be a dashboard of analytical data abuot your live streaming sessions. Things like concurrent viewers, total chat messages received, as well as other details. However, this will take some time to develop 😊.
 </template>
 
 <script>
