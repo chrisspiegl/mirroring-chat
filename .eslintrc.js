6a6f0c065c5df9c8a8b26e2cb3f37cfd7abe02e1
Chris Spiegl <chris@chrisspiegl.com>
2020-05-27 12:16:42 +0200

    Update formatting settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -16,8 +13,26 @@ module.exports = {
     semi: ['warn', 'never'],
     'no-unused-vars': 'warn',
     'max-len': 0,
+    'vue/max-attributes-per-line': 'off',
     radix: ['error', 'as-needed'],
-    'func-names': ['error', 'as-needed', { generators: 'as-needed' }],
+    'func-names': ['error', 'as-needed', {
+      generators: 'as-needed',
+    }],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          multiline: true,
+          minProperties: 1,
+        },
+        ObjectPattern: {
+          multiline: true,
+          minProperties: 9999999,
+        },
+        ImportDeclaration: 'never',
+        ExportDeclaration: 'always',
+      },
+    ],
   },
   settings: {
     'import/resolver': {
