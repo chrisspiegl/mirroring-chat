ad7df02e2200ac562ce76c1585ef8071bfe8dbfe
Chris Spiegl <chris@chrisspiegl.com>
2020-05-28 11:55:20 +0200

    Backend: Twitch: implement basic markdown parsing and html sanitization based on coding garden twitch bot implementation

--- server/messagesStore.js
+++ server/messagesStore.js
@@ -9,11 +9,16 @@ const error = debug(`${config.slug}:messagesStore:error`)
 
 const Filter = require('bad-words') // package to manage bad word filtering
 const naughtyWords = require('naughty-words') // shutterstock bad word lists
+const MarkdownIt = require('markdown-it')
+
+const marked = require('marked')
+const DOMPurify = require('dompurify')
 
 const RedisPubSubManager = require('server/redisPubSubManager')
 const redisKeyGenerator = require('server/redisKeyGenerator')
 const models = require('database/models')
 
+const md = new MarkdownIt()
 const naughtyWordsCustom = [] // set array of custom bad words that I want to filter
 const badWordFilter = new Filter({ placeHolder: '🌸', list: [naughtyWordsCustom, ...Object.values(naughtyWords)].flat() }) // set placeholder & combine all bad word lists to one and add it to the bad word filter
 const messageSubscribers = {}
@@ -51,6 +56,13 @@ const addMessage = async (_message) => {
   log('addNewMessage -> message', _message)
   const message = { ..._message }
   message.message = badWordFilter.clean(message.message)
+  // TODO: replace custom emotes see https://github.com/CodingGarden/live-chat-manager/blob/master/server/src/routes/index.js#L339
+  // The replace has to happen before the markdown because the replace utilizes the markdown image tag.
+  message.message = DOMPurify.sanitize(marked(message), {
+    FORBID_ATTR: ['style', 'onerror', 'onload'],
+    FORBID_TAGS: ['table', 'script', 'audio', 'video', 'style', 'iframe', 'textarea', 'frame', 'frameset'],
+  })
+
   await models.ChatMessage.create(message)
   rpsm.publish(redisKeyGenerator.messages.stream(message.idUser), message)
   rpsm.publish(redisKeyGenerator.events, {
