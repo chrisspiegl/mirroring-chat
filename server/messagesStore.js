b09b93ba3441ac0d9801c1261b02969d096274af
Chris Spiegl <chris@chrisspiegl.com>
2020-05-29 13:09:39 +0200

    - Test and Fix Markdown parsing - Test and Fix Twitch Emotes - Test and Fix Bad Word Filter

--- server/messagesStore.js
+++ server/messagesStore.js
@@ -9,20 +9,27 @@ const error = debug(`${config.slug}:messagesStore:error`)
 
 const Filter = require('bad-words') // package to manage bad word filtering
 const naughtyWords = require('naughty-words') // shutterstock bad word lists
-const MarkdownIt = require('markdown-it')
-
 const marked = require('marked')
-const DOMPurify = require('dompurify')
+const createDOMPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
 
 const RedisPubSubManager = require('server/redisPubSubManager')
 const redisKeyGenerator = require('server/redisKeyGenerator')
 const models = require('database/models')
 
-const md = new MarkdownIt()
+const { window } = new JSDOM('')
 const naughtyWordsCustom = [] // set array of custom bad words that I want to filter
 const badWordFilter = new Filter({ placeHolder: '🌸', list: [naughtyWordsCustom, ...Object.values(naughtyWords)].flat() }) // set placeholder & combine all bad word lists to one and add it to the bad word filter
 const messageSubscribers = {}
 const rpsm = new RedisPubSubManager()
+
+const DOMPurify = createDOMPurify(window)
+DOMPurify.addHook('afterSanitizeAttributes', (node) => {
+  // A way to implement a automatic CDN usage in the purify process by replacing all src attributes of images for example
+  if (node.hasAttribute('src')) {
+    node.setAttribute('src', `https://yacdn.org/serve/${node.getAttribute('src')}`)
+  }
+})
 
 const fetchByUser = async (idUser, limit = 50, offset = 0) => {
   log('fetchForUser -> idUser', idUser)
@@ -58,7 +65,7 @@ const addMessage = async (_message) => {
   message.message = badWordFilter.clean(message.message)
   // TODO: replace custom emotes see https://github.com/CodingGarden/live-chat-manager/blob/master/server/src/routes/index.js#L339
   // The replace has to happen before the markdown because the replace utilizes the markdown image tag.
-  message.message = DOMPurify.sanitize(marked(message), {
+  message.message = DOMPurify.sanitize(marked(message.message), {
     FORBID_ATTR: ['style', 'onerror', 'onload'],
     FORBID_TAGS: ['table', 'script', 'audio', 'video', 'style', 'iframe', 'textarea', 'frame', 'frameset'],
   })
