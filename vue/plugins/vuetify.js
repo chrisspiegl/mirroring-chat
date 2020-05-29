fa0f9452f9aa678b7843754e6ac399e1601ea0d1
Chris Spiegl <chris@chrisspiegl.com>
2020-05-29 13:15:01 +0200

    add important individual components so that they are found by the snackbar / confirm dialog components.

--- vue/plugins/vuetify.js
+++ vue/plugins/vuetify.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
 import CUSTOM_ICONS from './fontawesome'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  components: {
+    VSnackbar,
+    VBtn,
+    VIcon,
+  },
+})
 
 const opts = {
+  theme: {
+    dark: true,
+    themes: {},
+  },
   icons: {
+    // iconfont: 'faSvg',
     iconfont: 'faSvg',
     values: CUSTOM_ICONS,
   },
