582322cec1c41a0f4d8de5e8fcef5a7a2d40d650
Chris Spiegl <chris@chrisspiegl.com>
2020-06-03 15:57:05 +0200

    Display number of unacknowledged chat messages in the sidebar.

--- vue/components/navigation/index.vue
+++ vue/components/navigation/index.vue
@@ -2,7 +2,7 @@
   div
     v-app-bar(app, clipped-left, dense)
       //- TODO: remember the state of the menu in the users local storage? (eventually once there is a unified global 'settings' page or such)
-      v-app-bar-nav-icon(@click.stop="miniVariant = !miniVariant")
+      v-app-bar-nav-icon(@click.stop="interfaceHasSidebarClosed = !interfaceHasSidebarClosed")
       v-toolbar-title mirroring.chat
         = ' '
         em(style="font-size: .7rem;") v{{version}}
@@ -10,10 +10,10 @@
       v-toolbar-items.hidden-sm-and-down
         v-btn(to="/home") Home
         //- v-btn(to="/about") About
-        v-btn(to="/login", v-if="!loggedIn") Login
-        v-btn(to="/logout", v-if="loggedIn") Logout
+        v-btn(v-if="!loggedIn", to="/login") Login
+        v-btn(v-if="loggedIn", @click="actionLogOut") Logout
 
-    v-navigation-drawer(v-model="miniVariant", app, top, clipped, :permanent="!isMobile", :mini-variant="miniVariant && !isMobile")
+    v-navigation-drawer(v-model="interfaceHasSidebarClosed", app, top, clipped, :permanent="!isMobile", :mini-variant="interfaceHasSidebarClosed && !isMobile")
       vue-scroll
         v-list(nav, dense)
           v-list-item(link, to="/home", :class="(loggedIn && activated ? 'hidden-md-and-up' : '')")
@@ -44,7 +44,8 @@
 
           v-list-item(v-if="loggedIn && activated", link, to="/chat")
             v-list-item-action
-              v-icon(title="Chat") $chat
+              v-badge(color="green", :value="chatMessagesUndone.length", :content="(chatMessagesUndone.length < 10) ? chatMessagesUndone.length : '9+'")
+                v-icon(title="Chat") $chat
             v-list-item-content
               v-list-item-title Chat
 
@@ -62,7 +63,7 @@
           //-   v-list-item-content
           //-     v-list-item-title Settings
 
-          v-list-item.hidden-md-and-up(v-if="loggedIn", link, to="/logout")
+          v-list-item.hidden-md-and-up(v-if="loggedIn", @click="actionLogOut")
             v-list-item-action
               v-icon(title="Logout") $logout
             v-list-item-content
@@ -77,40 +78,42 @@
               v-list-item-title Support/Help
       template(v-slot:append)
         v-btn.py-8(block href="https://ChrisSpiegl.com" title="Made by Chris" target="_self")
-          v-icon(v-if="miniVariant") $madeBy
-          span(v-if="!miniVariant") Made with ❤ by Chris
-        //- v-list
-        //-   //- div Made by #[a(href="https://ChrisSpiegl.com") Chris Spiegl]
-        //-   v-list-item.hidden-md-and-up(v-if="loggedIn && activated", link, to="/logout")
-        //-       v-list-item-action
-        //-         v-icon(title="Logout") $logout
-        //-       v-list-item-content
-        //-         v-list-item-title Logout
+          v-icon(v-if="interfaceHasSidebarClosed") $madeBy
+          span(v-if="!interfaceHasSidebarClosed") Made with ❤ by Chris
 
 </template>
 
 <script>
 
 import { mapState } from 'vuex'
-import { authComputed } from '@/state/helpers'
+import { authComputed, authMethods, chatMessagesComputed } from '@/state/helpers'
 
 
 export default {
   name: 'navigation',
+  data() {
+    return {
+      interfaceHasSidebarClosed: true,
+    }
+  },
   computed: {
     ...authComputed,
+    ...chatMessagesComputed,
     ...mapState({
       version: (state) => state.version,
-      authLoading: (state) => state.auth.status === 'loading',
-      displayName: (state) => `${state.user.profile.displayName}`,
       isMobile: (state) => state.isMobile,
     }),
   },
-
-  data: () => ({
-    miniVariant: false,
-  }),
-
+  methods: {
+    ...authMethods,
+    actionLogOut() {
+      this.$router.push({ name: 'home' }).then(() => {
+        this.logOut().then(() => {
+          console.log('action Log Out Success')
+        })
+      })
+    },
+  },
   mounted() {
     this.$log.debug('components/navigation/index.vue mounted')
   },
